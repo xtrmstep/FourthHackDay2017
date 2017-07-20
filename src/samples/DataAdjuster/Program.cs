4094c532a897242924b6b4f4b83a7c24831c8eb6
Alexander Goida <alexander.goida@gmail.com>
2017-07-20 12:40:13 +0300

    Improved version of the data cleaner

--- src/samples/DataAdjuster/Program.cs
+++ src/samples/DataAdjuster/Program.cs
@@ -1,42 +1,71 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace datacleaner
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            const int columnNumber = 7;
+            const string OUTPUT_FILE = "output.csv";
+            const string COLUMNS = "Locationid,RecipeName,PLU,Salesdate,Quantity,NetSalesPrice,CostPrice,Year,Month,Day,WeekDay,YearDay";
+            const int COLUMN_NUMBER = 7;
             var files = args;
+
+            File.AppendAllLines(OUTPUT_FILE, new[] {COLUMNS}, Encoding.GetEncoding(1252)); // save in ANSI
+
             foreach (var file in files)
             {
-                var directoryName = Path.GetDirectoryName(file);
-                var fileName = Path.GetFileName(file).Replace(".csv", string.Empty); // removing extension for the name
-                var outputFile = directoryName + fileName + "_adj.csv";
-                var inputLines = File.ReadAllLines(file);
-                var outputLines = new string[inputLines.Length];
+                Console.WriteLine("Processing: " + file);
 
-                for (var index = 0; index < inputLines.Length; index++)
+                //var directoryName = Path.GetDirectoryName(file);
+                //var fileName = Path.GetFileName(file).Replace(".csv", string.Empty); // removing extension for the name
+                //var outputFile = directoryName + fileName + "_adj.csv";
+                Console.WriteLine("Loading...");
+                var inputLines = File.ReadAllLines(file);
+
+                var linesCount = inputLines.Length;
+                var outputLines = new string[linesCount];
+
+                for (var index = 0; index < linesCount; index++)
                 {
+                    Console.Write("\rline {0}/{1} ({2}%)", index, linesCount, Math.Round(index/(float) linesCount*100, 2));
+
                     var line = inputLines[index];
+
+                    #region remove all commas in the recipe name
+
                     var parts = line.Split(',');
-                    if (parts.Length > columnNumber)
+                    while (parts.Length > COLUMN_NUMBER)
                     {
                         // remove comma from name (it is the second comma in the line)
                         var commaPosition = line.IndexOf(',', 1);
-                        commaPosition = line.IndexOf(',', commaPosition+1);
+                        commaPosition = line.IndexOf(',', commaPosition + 1);
                         line = line.Remove(commaPosition, 1);
+                        parts = line.Split(',');
                     }
+
+                    #endregion
+
+                    #region add year-month-day
+
+                    parts = line.Split(',');
+                    var datetime = DateTime.Parse(parts[3]);
+                    line += string.Format("{0},{1},{2},{3},{4}", datetime.Year, datetime.Month, datetime.Day, (int)datetime.DayOfWeek, datetime.DayOfYear);
+
+                    #endregion
+
                     outputLines[index] = line;
                 }
+                Console.WriteLine("Processed");
 
-                File.WriteAllLines(outputFile, outputLines, Encoding.GetEncoding(1252)); // save in ansi
+                //File.WriteAllLines(file, outputLines, Encoding.GetEncoding(1252)); // save in ANSI
+
+
+                File.AppendAllLines(OUTPUT_FILE, outputLines, Encoding.GetEncoding(1252)); // save in ANSI
+                Console.WriteLine("Saved");
             }
         }
     }
