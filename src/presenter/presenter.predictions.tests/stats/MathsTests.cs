0b65aec5ad065793201529b855b31cafb6c04940
Alexander Goida <alexander.goida@gmail.com>
2017-07-24 18:48:08 +0300

    Add presenter version 2

---
+++ src/presenter/presenter.predictions.tests/stats/MathsTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using presenter.predictions.stats;
+using Xunit;
+
+namespace presenter.predictions.tests.stats
+{
+    public class MathsTests
+    {
+        [Fact]
+        public void MeanAbsoluteError_test()
+        {
+            var real = new double[] {4, 5, 10, 5, 7, 8, 4, 8, 2, 1, 2, 3, 5, 7, 3};
+            var estimated = new double[] {2, 10, 5, 7, 5, 4, 1, 2, 6, 3, 4, 7, 8, 9, 4};
+
+            const double EXPECTED = 3.133333333d;
+
+            var actual = Maths.MeanAbsoluteError(real, estimated);
+
+            Assert.True(Math.Abs(EXPECTED - actual) <= double.Epsilon);
+        }
+
+        [Fact]
+        public void MeanSquaredError_test()
+        {
+            var real = new double[] {4, 5, 10, 5, 7, 8, 4, 8, 2, 1, 2, 3, 5, 7, 3};
+            var estimated = new double[] {2, 10, 5, 7, 5, 4, 1, 2, 6, 3, 4, 7, 8, 9, 4};
+
+            const double EXPECTED = 11.8d;
+
+            var actual = Maths.MeanSquaredError(real, estimated);
+
+            Assert.True(Math.Abs(EXPECTED - actual) <= double.Epsilon);
+        }
+
+        [Fact]
+        public void Correlation_test()
+        {
+            var real = new double[] {4, 5, 10, 5, 7, 8, 4, 8, 2, 1, 2, 3, 5, 7, 3};
+            var estimated = new double[] {2, 10, 5, 7, 5, 4, 1, 2, 6, 3, 4, 7, 8, 9, 4};
+
+            const double EXPECTED = 0.093864155d;
+
+            var actual = Maths.Correlation(real, estimated);
+
+            Assert.True(Math.Abs(EXPECTED - actual) <= double.Epsilon);
+        }
+
+        [Fact]
+        public void CoefficientOfDetermination_test()
+        {
+            var real = new double[] {4, 5, 10, 5, 7, 8, 4, 8, 2, 1, 2, 3, 5, 7, 3};
+            var estimated = new double[] {2, 10, 5, 7, 5, 4, 1, 2, 6, 3, 4, 7, 8, 9, 4};
+
+            const double EXPECTED = 0.00881048d;
+
+            var actual = Maths.CoefficientOfDetermination(real, estimated);
+
+            Assert.True(Math.Abs(EXPECTED - actual) <= double.Epsilon);
+        }
+    }
+}
---
+++ src/presenter/presenter.predictions/stats/Maths.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq;
+
+namespace presenter.predictions.stats
+{
+    public static class Maths
+    {
+        /// <summary>
+        ///     Mean Absolute Error (MAE) measures how far predicted values are away from observed values.
+        /// </summary>
+        /// <remarks>
+        ///     The Mean Absolute Error (or MAE) is the sum of the absolute differences between predictions and actual values. It
+        ///     gives an idea of how wrong the predictions were. The measure gives an idea of the magnitude of the error, but no
+        ///     idea of the direction (e.g. over or under predicting).
+        ///     The less the better.
+        /// </remarks>
+        /// <returns></returns>
+        public static double MeanAbsoluteError(double[] actuals, double[] estimated)
+        {
+            var result = actuals.Select((t, i) => Math.Abs(t - estimated[i])).Sum();
+            result /= actuals.Length;
+            return Math.Round(result, 9);
+        }
+
+        /// <summary>
+        ///     The Mean Squared Error (or MSE) is much like the mean absolute error in that it provides a gross idea of the
+        ///     magnitude of error.
+        /// </summary>
+        /// <remarks>
+        ///     Taking the square root of the mean squared error converts the units back to the original units of the output
+        ///     variable and can be meaningful for description and presentation.This is called the Root Mean Squared Error(or
+        ///     RMSE).
+        /// </remarks>
+        /// <returns></returns>
+        public static double MeanSquaredError(double[] actuals, double[] estimated)
+        {
+            var result = actuals.Select((t, i) => Math.Pow(estimated[i] - t, 2)).Sum();
+            result /= actuals.Length;
+            return result;
+        }
+
+        /// <summary>
+        ///     The R^2 (or R Squared) metric provides an indication of the goodness of fit of a set of predictions to the actual
+        ///     values. In statistical literature, this measure is called the coefficient of determination.
+        /// </summary>
+        /// <remarks>
+        ///     This is a value between 0 and 1 for no-fit and perfect fit respectively. Less than 0.5 is the poor fit.
+        ///     Calculation: http://www.statisticshowto.com/what-is-a-coefficient-of-determination/
+        /// </remarks>
+        /// <returns></returns>
+        public static double CoefficientOfDetermination(double[] actuals, double[] estimated)
+        {
+            var correlationCoef = Correlation(actuals, estimated);
+            correlationCoef *= correlationCoef;
+            return Math.Round(correlationCoef, 9);
+        }
+
+        /// <summary>
+        ///     http://www.statisticshowto.com/how-to-compute-pearsons-correlation-coefficients/
+        /// </summary>
+        /// <param name="actuals"></param>
+        /// <param name="estimated"></param>
+        /// <returns></returns>
+        public static double Correlation(double[] actuals, double[] estimated)
+        {
+            var avg1 = actuals.Average();
+            var avg2 = estimated.Average();
+
+            var sum1 = actuals.Zip(estimated, (x1, y1) => (x1 - avg1)*(y1 - avg2)).Sum();
+
+            var sumSqr1 = actuals.Sum(x => Math.Pow((x - avg1), 2.0));
+            var sumSqr2 = estimated.Sum(y => Math.Pow((y - avg2), 2.0));
+
+            var result = sum1/Math.Sqrt(sumSqr1*sumSqr2);
+
+            return Math.Round(result, 9);
+        }
+    }
+}
